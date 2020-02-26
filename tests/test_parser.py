80f522eeb51eb9837f9dea2bd7abbc4622e7e54b
Matthew Carter <m.j.carter2@liverpool.ac.uk>
2020-02-26 16:42:40 +0000

    parsing log file

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,5 +2,13 @@ import sys
 sys.path.append('..')
 import condortools
 
-parser = condortools.Parser('log/log.log')
-parser.read()
+# Instantiate parser
+log_file = 'log/log.log'
+parser = condortools.Parser(log_file)
+
+print(parser.event_code(1)) # Test event codes
+
+latest_event = parser.parse_latest_event()
+print(latest_event)
+
+print(parser.parse_log_file())
